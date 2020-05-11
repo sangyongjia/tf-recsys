4ebeb4d390a36a56e3058654391fefba761ed4e2
桑永嘉 <sangyongjia@sangyongjiadeMacBook-Pro.local>
2020-05-11 16:18:38 +0800

    past test, will run on GPU for confirm

--- recommender/word2vec/word2vec.py
+++ recommender/word2vec/word2vec.py
@@ -1,3 +1,37 @@
+import pandas as pd
+import numpy as np
+import random
+import tensorflow as tf
+import math
+
+train_data = pd.read_csv("../../dataset/taobao_data/item2item.txt",names=['item','label'])
+
+files = "../../dataset/taobao_data/item2item.txt"
+column_names = ["item"]
+lalbel = [""]
+item = [""]
+
+def generate_batch(file_path=files, perform_shuffle=True, repeat_count=1,batch_size=1000):
+    def decode_csv(line):
+        parsed_line = tf.io.decode_csv(line, record_defaults=label+item, field_delim=',')
+        label = parsed_line[1]
+        del parsed_line[1]
+        features = parsed_line  # Everything but last elements are the features
+        d = dict(zip(column_names, features)), label
+        return d
+
+    dataset = (tf.data.TextLineDataset(file_path, num_parallel_reads=20)  # Read text file
+            .map(decode_csv,num_parallel_calls=20))  # Transform each elem by applying decode_csv fn
+    if perform_shuffle:
+    # Randomizes input using a window of 256 elements (read into memory)
+        dataset = dataset.shuffle(buffer_size=256*8*8)
+    dataset = dataset.repeat(repeat_count)  # Repeats dataset this # times
+    dataset = dataset.batch(batch_size=batch_size)  # Batch size to use
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+    dataset = dataset.make_one_shot_iterator()
+    features, labels = dataset.get_next()
+    return features, labels
 
 
 batch_size = 128
@@ -7,7 +41,7 @@ valid_size = 16
 valid_window = 100
 valid_example = np.array(random.sample(range(valid_window), valid_size))
 num_sampled = 64
-item_size = 15000
+item_size = 5163068
 
 graph = tf.Graph()
 with graph.as_default():
@@ -18,8 +52,8 @@ with graph.as_default():
 
     #定义变量
     embeddings = tf.Variable(tf.random_uniform([item_size,embedding_size],-1.0,1.0))
-    softmax_weight = tf.Variable(tf.truncated_normal([item_size, embedding_size],stddev=1.0/math.sqrt(embedding_size)))
-    softmax_baises = tf.Variable(tf.zeros([item_size]))
+    softmax_weights = tf.Variable(tf.truncated_normal([item_size, embedding_size],stddev=1.0/math.sqrt(embedding_size)))
+    softmax_biases = tf.Variable(tf.zeros([item_size]))
 
     #本次训练数据对应的embedding
     embed = tf.nn.embedding_lookup(embeddings, train_dataset)
@@ -38,15 +72,15 @@ with graph.as_default():
 
     ##train
     num_steps = 100000
-    with tf.Session(grap=grap) as session:
-        tf.global_variables_initialiazer().run()
+    with tf.Session(graph=graph) as session:
+        tf.global_variables_initializer().run()
         average_loss = 0 
         for step in range(num_steps+1):
-            batch_data, batch_labels = generate_batch()
+            batch_data, batch_labels = generate_batch(file_path=files, perform_shuffle=True, repeat_count=1,batch_size=100)
             feed_dict = {train_dataset:batch_data, train_labels:batch_labels}
             _,l = session.run([optimizer, loss], feed_dict=feed_dict)
             average_loss += 1
-            if step % 2000 ==0:
+            if step % 200 ==0:
                 if step > 0:
                     average_loss = average_loss / 2000
                 print("average_loss at step: {} is:{}".format(step, average_loss))
